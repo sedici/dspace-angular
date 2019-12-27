6c6211e8b6aded23fa08145fbe64de7db60b5276
Facundo Gabriel Adorno <adorno.facundo@gmail.com>
2019-12-27 10:12:06 -0300

    Fix para que funcione el live-reload en SystemD
    En ciertos casos, dependiendo de la configuración del sistema, el
    live-reload de Webpack no funciona. Esto se debe a limitaciones en la
    propiedad fs.inotify.max_user_watches. Este script incremente el valor
    de esta propiedad.

---
+++ sedici_bin/fix_live_reload.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+TARGET_FSINOTIFYMAXUSERWATCHES=524288
+
+cat <<-ENDF
+[INFO]
+This script is going to increment/modify your current system property of inotify watchers. This is neccessary to do, for example,
+when the live-reload function of Webpack don't work.
+For more information about:
+- https://stackoverflow.com/a/33537743/3406552
+- https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+[INFO]
+ENDF
+
+echo "First, please give me superuser root powers!"
+sudo echo "Done."
+echo  "You currently have `sudo sysctl fs.inotify.max_user_watches` amount set to 'fs.inotify.mas_user_watches'... Want to modify to $TARGET_FSINOTIFYMAXUSERWATCHES amount? [Y/n]"
+read answer
+
+if [ "$answer" == "Y" ] || [ "$answer" == "y"  ]; then
+    echo "Proceeding to temporary update the limit of 'fs.inotify.max_user_watches' amount to $TARGET_FSINOTIFYMAXUSERWATCHES."
+    sudo sysctl fs.inotify.max_user_watches=524288
+else
+    echo "Canceling operation. Exiting..."
+fi
