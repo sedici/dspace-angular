7dd408f87f16cbfcb59635184f5444376f13b47b
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2020-11-11 23:54:18 -0300

    Mejoras al grafico de publicaciones de un autor por tipo

--- src/app/entity-groups/research-entities/item-pages/person/sedici-services/person-statistics/publication-types-chart/publication-types-chart.component.ts
+++ src/app/entity-groups/research-entities/item-pages/person/sedici-services/person-statistics/publication-types-chart/publication-types-chart.component.ts
@@ -21,9 +21,9 @@ export class PublicationTypesChartComponent implements OnInit {
      {"Publication_type": "Image", "Number": "2"},
    ];
   private svg;
-  private margin = 20;
-  private width = 350;
-  private height = 200;
+  private margin = 40;
+  private width = 450;
+  private height = 450;
   // The radius of the pie chart is half the smallest side
   private radius = Math.min(this.width, this.height) / 2 - this.margin;
   private colors;
@@ -73,41 +73,82 @@ export class PublicationTypesChartComponent implements OnInit {
   private createColors(): void {
     this.colors = d3.scaleOrdinal()
     .domain(this.data.map(d => d.Number.toString()))
-    .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
+    .range(["#466cb0", "#304978", "#1f304e", "#7b97ca", "#cfd9ec"]);
   }
 
   private drawChart(): void {
     // Compute the position of each group on the pie:
     const pie = d3.pie<any>().value((d: any) => Number(d.Number));
 
+    var arc = d3.arc()
+      .innerRadius(this.radius * 0.5)         // This is the size of the donut hole
+      .outerRadius(this.radius * 0.8)
+
     // Build the pie chart
     this.svg
-    .selectAll('pieces')
+    .selectAll('allSlices')
     .data(pie(this.data))
     .enter()
     .append('path')
-    .attr('d', d3.arc()
-      .innerRadius(10)
-      .outerRadius(this.radius)
-    )
+    .attr('d', arc)
     .attr('fill', (d, i) => (this.colors(i)))
-    .attr("stroke", "#121926")
+    .attr("stroke", "white")
     .style("stroke-width", "1px");
 
-    // Add labels
-    const labelLocation = d3.arc()
-    .innerRadius(150)
-    .outerRadius(this.radius);
+    // // Add labels
+    // const labelLocation = d3.arc()
+    //  .innerRadius(this.radius * 0.2)
+    //  .outerRadius(this.radius);
 
+    // this.svg
+    //  .selectAll('pieces')
+    //  .data(pie(this.data))
+    //  .enter()
+    //  .append('text')
+    //  .text(d => d.data.Number)
+    //  .attr("transform", d => "translate(" + labelLocation.centroid(d) + ")")
+    //  //.style("text-anchor", "middle")
+    //  .style("font-size", 9);
+
+    // Another arc that won't be drawn. Just for labels positioning
+    var outerArc = d3.arc()
+    .innerRadius(this.radius)
+    .outerRadius(this.radius)
+
+    // Add the polylines between chart and labels:
     this.svg
-    .selectAll('pieces')
-    .data(pie(this.data))
-    .enter()
-    .append('text')
-    .text(d => d.data.Publication_type + " " + d.data.Number)
-    .attr("transform", d => "translate(" + labelLocation.centroid(d) + ")")
-    .style("text-anchor", "middle")
-    .style("font-size", 9);
+      .selectAll('allPolylines')
+      .data(pie(this.data))
+      .enter()
+      .append('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function(d) {
+          var posA = arc.centroid(d) // line insertion in the slice
+          var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+          var posC = outerArc.centroid(d); // Label position = almost the same as posB
+          var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+          posC[0] = this.radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+          return [posA, posB, posC]
+        })
 
+    // Add the polylines between chart and labels:
+    this.svg
+      .selectAll('allLabels')
+      .data(pie(this.data))
+      .enter()
+      .append('text')
+        .text( function(d) { return d.data.Publication_type  } )
+        .attr('transform', function(d) {
+            var pos = arc.centroid(d);
+            // var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            // pos[0] = this.radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function(d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
   }
 }
