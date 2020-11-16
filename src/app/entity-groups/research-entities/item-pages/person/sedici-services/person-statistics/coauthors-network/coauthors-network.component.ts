24727e9ad1b41a547e82c7616648fea1245f7c6d
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2020-11-15 22:53:50 -0300

    Mejoro el grafico de red de coautores en las estadisticas de un autor 	Los datos siguen harcodeados

--- src/app/entity-groups/research-entities/item-pages/person/sedici-services/person-statistics/coauthors-network/coauthors-network.component.ts
+++ src/app/entity-groups/research-entities/item-pages/person/sedici-services/person-statistics/coauthors-network/coauthors-network.component.ts
@@ -12,64 +12,150 @@ import { parse } from 'echarts/extension/dataTool/gexf';
 })
 export class CoauthorsNetworkComponent implements OnInit {
 
-  options: Observable<any>;
+  options; //Observable<any>;
+
+  data = {
+    "nodes": [
+      {
+        "id": 1,
+        "name": "Carlos"
+      },
+      {
+        "id": 2,
+        "name": "Ernesto"
+      },
+      {
+        "id": 3,
+        "name": "Gloria"
+      },
+      {
+        "id": 4,
+        "name": "Ana"
+      },
+      {
+        "id": 5,
+        "name": "Miguel"
+      },
+      {
+        "id": 6,
+        "name": "Fernando"
+      },
+      {
+        "id": 7,
+        "name": "Gonzalo"
+      },
+      {
+        "id": 8,
+        "name": "Lucia"
+      },
+      {
+        "id": 9,
+        "name": "Eliana"
+      },
+      {
+        "id": 10,
+        "name": "Juan"
+      }
+    ],
+    "links": [
+      {
+        "source": 1,
+        "target": 2
+      },
+      {
+        "source": 1,
+        "target": 5
+      },
+      {
+        "source": 1,
+        "target": 6
+      },
+      {
+        "source": 2,
+        "target": 3
+      },
+              {
+        "source": 2,
+        "target": 7
+      }
+      ,
+      {
+        "source": 3,
+        "target": 4
+      },
+       {
+        "source": 8,
+        "target": 3
+      }
+      ,
+      {
+        "source": 4,
+        "target": 5
+      }
+      ,
+      {
+        "source": 4,
+        "target": 9
+      },
+      {
+        "source": 5,
+        "target": 10
+      }
+    ]
+  }
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.options = this.http.get('https://raw.githubusercontent.com/xieziyu/ngx-echarts/master/src/assets/data/les-miserables.gexf', { responseType: 'text' }).pipe(
-      map((xml) => {
-        const graph = parse(xml);
-        const categories = [];
-        for (let i = 0; i < 9; i++) {
-          categories[i] = {
-            name: 'category' + i,
-          };
-        }
-        graph.nodes.forEach((node) => {
-          node.itemStyle = null;
-          node.symbolSize = 10;
-          node.value = node.symbolSize;
-          node.category = node.attributes.modularity_class;
-          // Use random x, y
-          node.x = node.y = null;
-          node.draggable = true;
-        });
-        return {
-          title: {
-            text: 'Les Miserables',
-            subtext: 'Default layout',
-            top: 'bottom',
-            left: 'right',
+
+    const categories = [];
+    var index = 0;
+    this.data.nodes.forEach((node) => {
+      node['itemStyle'] = null;
+      node['symbolSize']  = 10;
+      categories[index] = {
+        name: node.name,
+      };
+      index += 1;
+      //node['value'] = node['symbolSize'];
+      node['category'] = index;
+      // Use random x, y
+      node['x'] = node['y'] = null;
+      node['draggable'] = true;
+    });
+    this.options = {
+      title: {
+        text: 'CoAutores',
+        subtext: '',
+        top: 'bottom',
+        left: 'right',
+      },
+      tooltip: {},
+      legend: [
+        {
+          data: categories.map((a) => a.name),
+        },
+      ],
+      animation: false,
+      series: [
+        {
+          name: 'Coautores',
+          type: 'graph',
+          layout: 'force',
+          data: this.data.nodes,
+          links: this.data.links,
+          categories,
+          roam: true,
+          label: {
+            normal: {
+              position: 'right',
+            },
           },
-          tooltip: {},
-          legend: [
-            {
-              data: categories.map((a) => a.name),
-            },
-          ],
-          animation: false,
-          series: [
-            {
-              name: 'Les Miserables',
-              type: 'graph',
-              layout: 'force',
-              data: graph.nodes,
-              links: graph.links,
-              categories,
-              roam: true,
-              label: {
-                normal: {
-                  position: 'right',
-                },
-              },
-              force: {
-                repulsion: 100,
-              },
-            },
-          ],
-        };
-      }),
-    );
+          force: {
+            repulsion: 100,
+          },
+        },
+      ],
+    };
   }
 }
