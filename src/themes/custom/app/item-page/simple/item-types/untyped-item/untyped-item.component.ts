20ea709df8e0d5eaff4f19bb18c460698c21d64f
Felipe Mosqueira <mosqueirafelipe22@gmail.com>
2024-02-27 13:03:03 -0300

    Vista simple del ítem

--- src/themes/custom/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ src/themes/custom/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -15,10 +15,10 @@ import {
 @listableObjectComponent(Item, ViewMode.StandalonePage, Context.Any, 'custom')
 @Component({
   selector: 'ds-untyped-item',
-  // styleUrls: ['./untyped-item.component.scss'],
-  styleUrls: ['../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss'],
-  // templateUrl: './untyped-item.component.html',
-  templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
+  styleUrls: ['./untyped-item.component.scss'],
+  // styleUrls: ['../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss'],
+  templateUrl: './untyped-item.component.html',
+  // templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UntypedItemComponent extends BaseComponent {
