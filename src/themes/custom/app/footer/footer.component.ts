d1e5372035f891f3920c9f75ddca326fdf428919
178Pelado <mosqueirafelipe22@gmail.com>
2025-05-09 12:26:31 -0300

    11006 cambios footer y primera version mobile

--- src/themes/custom/app/footer/footer.component.ts
+++ src/themes/custom/app/footer/footer.component.ts
@@ -3,11 +3,17 @@ import {
   DatePipe,
   NgIf,
 } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { HostWindowService, WidthCategory } from 'src/app/shared/host-window.service';
 
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
+import { KlaroService } from 'src/app/shared/cookies/klaro.service';
+import { AuthorizationDataService } from 'src/app/core/data/feature-authorization/authorization-data.service';
+import { NotifyInfoService } from 'src/app/core/coar-notify/notify-info/notify-info.service';
+import { APP_CONFIG, AppConfig } from 'src/config/app-config.interface';
 
 @Component({
   selector: 'ds-themed-footer',
@@ -19,4 +25,21 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
   imports: [NgIf, RouterLink, AsyncPipe, DatePipe, TranslateModule],
 })
 export class FooterComponent extends BaseComponent {
+  public isMobile$: Observable<boolean>;
+  
+  maxMobileWidth = WidthCategory.SM;
+
+  constructor(
+    protected windowService: HostWindowService,
+    @Optional() public cookies: KlaroService,
+    protected authorizationService: AuthorizationDataService,
+    protected notifyInfoService: NotifyInfoService,
+    @Inject(APP_CONFIG) protected appConfig: AppConfig
+  ) {
+    super(cookies, authorizationService, notifyInfoService, appConfig);
+  }
+
+  ngOnInit(): void {
+    this.isMobile$ = this.windowService.isUpTo(this.maxMobileWidth);
+  }
 }
