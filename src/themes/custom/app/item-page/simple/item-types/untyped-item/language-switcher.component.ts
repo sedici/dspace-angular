d7fa94d5825406fae76f22260b76195aab2af157
178Pelado <mosqueirafelipe22@gmail.com>
2025-06-06 09:46:13 -0300

    soluciono error vista de abstracts sin lang asignado

--- src/themes/custom/app/item-page/simple/item-types/untyped-item/language-switcher.component.ts
+++ src/themes/custom/app/item-page/simple/item-types/untyped-item/language-switcher.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { NgIf, NgFor } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TruncatableComponent } from 'src/app/shared/truncatable/truncatable.component';
 import { TruncatablePartComponent } from 'src/app/shared/truncatable/truncatable-part/truncatable-part.component';
 
@@ -22,24 +22,29 @@ export class LanguageSwitcherComponent {
   @Input() item: any;
   selectedLanguage: string;
   availableLanguages: any[];
+  abstractPrefix: string;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private translateService: TranslateService ) {}
 
   ngOnInit() {
-    this.selectedLanguage = this.item.metadata['dc.language']?.[0]?.value || 'es';
+    const langValue = this.item.metadata['dc.language']?.[0]?.value;
+    this.selectedLanguage = !langValue || langValue === 'other' ? '??' : langValue;
     this.availableLanguages = this.getAvailableLanguages();
+
+    this.translateService.get('item.page.abstract').subscribe((res: string) => {
+      this.abstractPrefix = res + ': ';
+    });
   }
 
   getAbstract(): SafeHtml {
     const abstracts = this.item.metadata['dc.description.abstract'];
     if (abstracts) {
-      let abstract = abstracts.find((abstract: any) => (abstract.language || 'es') === this.selectedLanguage)?.value || '';
+      let abstract = abstracts.find((abstract: any) => (abstract.language || '??') === this.selectedLanguage)?.value || '';
       if (!abstract) {
         abstract = abstracts[0].value;
         this.selectedLanguage = abstracts[0].language;
       }
-      const prefix = 'Resumen: ';
-      abstract = prefix + abstract;
+      abstract = this.abstractPrefix + abstract;
       return this.sanitizer.bypassSecurityTrustHtml(abstract);
     }
     return this.sanitizer.bypassSecurityTrustHtml('');
@@ -52,7 +57,7 @@ export class LanguageSwitcherComponent {
   getAvailableLanguages() {
     const abstracts = this.item.metadata['dc.description.abstract'];
     if (abstracts) {
-      return [...new Set(abstracts.map((abstract: any) => abstract.language))];
+      return [...new Set(abstracts.map((abstract: any) => (abstract.language || '??')))];
     }
     return [];
   }
