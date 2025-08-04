ef3a53634b7b415585ab5e386a22ccfbb569d529
Felipe Mosqueira <mosqueirafelipe22@gmail.com>
2025-08-03 23:03:20 -0300

    Primera versión acomodada de los filtros. Asociados a inpust y asociados al texto seleccionado

---
+++ src/app/submission/models/filter-config.model.ts
@@ -0,0 +1,28 @@
+export interface FilterInfo {
+  id: string;
+  label?: string;
+  icon?: string;
+  description: string;
+}
+
+export class FilterConfig {
+  static readonly GENERAL_METADATA_FILTER: FilterInfo[] = [
+    { id: 'upperCase', label: 'AA', description: 'upperCase\nPasa el texto completo a mayúsculas' },
+    { id: 'lowerCase', label: 'aa', description: 'lowerCase\nPasa el texto completo a minúsculas' },
+    { id: 'capitalize', label: 'Aa', description: 'Capitalize\nPasa la primera letra de cada oración a mayúscula' },
+    { id: 'removeDoubleSpaces', label: '❌␣␣', description: 'Remove double spaces\nElimina los espacios dobles' },
+    { id: 'removeSpacesBetweenLetters', icon: 'fa-solid fa-bars', description: 'Remove spaces between letters\nSaca espacios de donde no van (Ej: T I T U L O)' },
+    { id: 'removeSpacesAtStartAndEnd', icon: 'fa-solid fa-bars', description: 'Remove spaces at start and end\nElimina los espacios al principio y al final del texto' },
+    { id: 'removeLineBreaks', icon: 'fa-solid fa-bars', description: 'Remove line breaks\nElimina los saltos de línea' }
+  ];
+
+  static readonly REPEATABLE_METADATA_FILTER: FilterInfo[] = [
+    { id: 'splitByDelimiter', label: 'Split', description: 'splitByDelimiter\nSe toma el texto completo como una lista y se separa en cada uno de sus elementos' },
+  ];
+
+  static readonly PEOPLE_METADATA_FILTER: FilterInfo[] = [
+    { id: 'reorderPerson', icon: 'fa-solid fa-right-left', description: 'Reorder person name\nReodena un "Nombre Apellido" en "Apellido, Nombre" (SOLO SIRVE CON UNO DE CADA UNO)' },
+    { id: 'removeTitles', label: '❌🎓', description: 'Remove titles\nElimina títulos o grados de las personas (Ej: Lic., Mg.)' },
+    { id: 'removeReferences', label: '❌🔢', description: 'Remove references\nElimina referencias asociadas de las personas (Ej: números, asteriscos)' },
+  ];
+}
