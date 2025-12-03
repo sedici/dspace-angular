e8bac772829152c2d1c2cfc6a36c18ca09d6608f
Santiago Fierro <santiago.fierro99@gmail.com>
2025-12-03 08:50:58 -0300

    Inhabilito funcionalidad de request-a-copy

--- src/themes/custom/app/item-page/simple/item-types/untyped-item/sedici-file-download-link.component.ts
+++ src/themes/custom/app/item-page/simple/item-types/untyped-item/sedici-file-download-link.component.ts
@@ -1,23 +1,13 @@
 import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
-import {
-  ActivatedRoute,
-  RouterLink,
-} from '@angular/router';
-import {
-  TranslateModule,
-  TranslateService,
-} from '@ngx-translate/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FileSizePipe } from 'src/app/shared/utils/file-size-pipe';
 import { FileDownloadLinkComponent } from 'src/app/shared/file-download-link/file-download-link.component';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthorizationDataService } from 'src/app/core/data/feature-authorization/authorization-data.service';
 import { ChangeDetectorRef } from '@angular/core';
-import {
-  combineLatest as observableCombineLatest,
-  Observable,
-  of,
-} from 'rxjs';
+import { combineLatest as observableCombineLatest, Observable, of } from 'rxjs';
 import { FeatureID } from 'src/app/core/data/feature-authorization/feature-id';
 import { map } from 'rxjs/operators';
 import { isNotEmpty } from 'src/app/shared/empty.util';
@@ -33,12 +23,19 @@ import { DSONameService } from 'src/app/core/breadcrumbs/dso-name.service';
     RouterLink,
     TranslateModule,
     FileSizePipe,
-    NgbTooltipModule
-],
+    NgbTooltipModule,
+  ],
 })
-export class SediciFileDownloadLinkComponent extends FileDownloadLinkComponent implements OnInit {
+export class SediciFileDownloadLinkComponent
+  extends FileDownloadLinkComponent
+  implements OnInit
+{
   @Input() isSticky: boolean = false;
   @Input() isMobile: boolean = false;
+  // SEDICI:  Este input es para controlar si el archivo es descargable o no
+  // Se agregó para inhabilitar el módulo de request a copy
+  // En caso de querer habilitarlo se debe eliminar
+  @Input() canDownload: boolean = false;
 
   constructor(
     protected authorizationService: AuthorizationDataService,
@@ -50,46 +47,80 @@ export class SediciFileDownloadLinkComponent extends FileDownloadLinkComponent i
     super(authorizationService, dsoNameService, route, translateService);
   }
 
+  ngOnInit(): void {
+    this.enableRequestACopy = false;
+    super.ngOnInit();
+  }
+
   ngOnChanges() {
     this.getBitstreamPathObservable();
     this.cdr.detectChanges(); // Forzar la detección de cambios
   }
-  
+
   private getBitstreamPathObservable(): void {
     if (this.enableRequestACopy) {
       this.itemRequest = this.route.snapshot.data.itemRequest;
-      this.canDownload$ = this.authorizationService.isAuthorized(FeatureID.CanDownload, isNotEmpty(this.bitstream) ? this.bitstream.self : undefined);
-      this.canDownloadWithToken$ = of((this.itemRequest && this.itemRequest.acceptRequest && !this.itemRequest.accessExpired) ? (this.itemRequest.allfiles !== false || this.itemRequest.bitstreamId === this.bitstream.uuid) : false);
-      this.canRequestACopy$ = this.authorizationService.isAuthorized(FeatureID.CanRequestACopy, isNotEmpty(this.bitstream) ? this.bitstream.self : undefined);
-       this.bitstreamPath$ = observableCombineLatest([this.canDownload$, this.canDownloadWithToken$, this.canRequestACopy$]).pipe(
-        map(([canDownload, canDownloadWithToken, canRequestACopy]) => this.getBitstreamPath(canDownload, canDownloadWithToken, canRequestACopy)),
+      this.canDownload$ = this.authorizationService.isAuthorized(
+        FeatureID.CanDownload,
+        isNotEmpty(this.bitstream) ? this.bitstream.self : undefined
+      );
+      this.canDownloadWithToken$ = of(
+        this.itemRequest &&
+          this.itemRequest.acceptRequest &&
+          !this.itemRequest.accessExpired
+          ? this.itemRequest.allfiles !== false ||
+              this.itemRequest.bitstreamId === this.bitstream.uuid
+          : false
+      );
+      this.canRequestACopy$ = this.authorizationService.isAuthorized(
+        FeatureID.CanRequestACopy,
+        isNotEmpty(this.bitstream) ? this.bitstream.self : undefined
+      );
+      this.bitstreamPath$ = observableCombineLatest([
+        this.canDownload$,
+        this.canDownloadWithToken$,
+        this.canRequestACopy$,
+      ]).pipe(
+        map(([canDownload, canDownloadWithToken, canRequestACopy]) =>
+          this.getBitstreamPath(
+            canDownload,
+            canDownloadWithToken,
+            canRequestACopy
+          )
+        )
       );
     } else {
       this.bitstreamPath$ = of(this.getBitstreamDownloadPath());
-      this.canDownload$ = of(true);
+      this.canDownload$ = of(false);
     }
   }
 
   public isFileDownloadable(): Observable<boolean> {
-    return this.authorizationService.isAuthorized(FeatureID.CanDownload, 
-      isNotEmpty(this.bitstream) ? this.bitstream.self : undefined);
+    return this.authorizationService.isAuthorized(
+      FeatureID.CanDownload,
+      isNotEmpty(this.bitstream) ? this.bitstream.self : undefined
+    );
   }
 
-  adaptFileSize (size: string): string {
-    if (size.includes("KB")) {
-      const kbValue = parseFloat(size.replace("KB", "").trim()); // Extraer el valor numérico en KB
+  adaptFileSize(size: string): string {
+    if (size.includes('KB')) {
+      const kbValue = parseFloat(size.replace('KB', '').trim()); // Extraer el valor numérico en KB
       const mbValue = kbValue / 1024; // Convertir a MB y formatear a 1 decimal
       const roundedMbValue = Math.round(mbValue * 10) / 10; // Redondear a 1 decimal
-      const formattedMbValue = roundedMbValue % 1 === 0 ? roundedMbValue.toFixed(0) : roundedMbValue.toFixed(1); // Verificar si es un valor entero
+      const formattedMbValue =
+        roundedMbValue % 1 === 0
+          ? roundedMbValue.toFixed(0)
+          : roundedMbValue.toFixed(1); // Verificar si es un valor entero
       return `${formattedMbValue} MB`;
-    } else if (size.includes("MB")) {
-      const mbValue = parseFloat(size.replace("MB", "").trim()); // Extraer el valor numérico en MB
-      if (mbValue >= 1000) { // Si el valor en MB es mayor o igual a 1000, convertir a GB
+    } else if (size.includes('MB')) {
+      const mbValue = parseFloat(size.replace('MB', '').trim()); // Extraer el valor numérico en MB
+      if (mbValue >= 1000) {
+        // Si el valor en MB es mayor o igual a 1000, convertir a GB
         // const gbValue = mbValue / 1000;
         // const roundedGbValue = Math.round(gbValue * 10) / 10;
         // const formattedGbValue = roundedGbValue % 1 === 0 ? roundedGbValue.toFixed(0) : roundedGbValue.toFixed(1);
         // return `${formattedGbValue} GB`;
-        return "1 GB"; // Todavía en SEDICI no hay archivos que superen 1 GB, pero queda plateado a futuro el método
+        return '1 GB'; // Todavía en SEDICI no hay archivos que superen 1 GB, pero queda plateado a futuro el método
       }
     }
     return size;
