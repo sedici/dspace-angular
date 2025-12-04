e889110f696f4b6d38e69344fee6525a14a2216e
Santiago Fierro <santiago.fierro99@gmail.com>
2025-12-04 11:38:20 -0300

    Subo correcciones en los nombres de los selectores

--- src/themes/custom/app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component.ts
+++ src/themes/custom/app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 import { CommunityPageSubCollectionListComponent as BaseComponent } from '../../../../../../../app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component';
 
 @Component({
-  selector: 'ds-community-page-sub-collection-list',
+  selector: 'ds-themed-community-page-sub-collection-list',
+  template: '', // SEDICI: El siguiente código fue comentado para ocultar la lista de colecciones
+  // debido a la modificación de la visualización de la colección en modo de tree
   // styleUrls: ['./community-page-sub-collection-list.component.scss'],
-  template: '', // Empty template to hide the list
+  // styleUrls: ['../../../../../../../app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component.scss'],
+  //templateUrl: './community-page-sub-collection-list.component.html',
+  // templateUrl: '../../../../../../../app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component.html',
+  // imports: [
+  //   AsyncPipe,
+  //   ErrorComponent,
+  //   ObjectCollectionComponent,
+  //   ThemedLoadingComponent,
+  //   TranslateModule,
+  //   VarDirective,
+  // ],
   standalone: true,
   imports: [],
 })
--- src/themes/custom/app/community-page/sections/sub-com-col-section/sub-community-list/community-page-sub-community-list.component.ts
+++ src/themes/custom/app/community-page/sections/sub-com-col-section/sub-community-list/community-page-sub-community-list.component.ts
@@ -82,9 +82,11 @@ export class SubComColDatasource implements DataSource<FlatNode> {
 }
 
 @Component({
-  selector: 'ds-community-page-sub-community-list',
+  selector: 'ds-themed-community-page-sub-community-list',
   styleUrls: ['./community-page-sub-community-list.component.scss'],
+  // styleUrls: ['../../../../../../../app/community-page/sections/sub-com-col-section/sub-community-list/community-page-sub-community-list.component.scss'],
   templateUrl: './community-page-sub-community-list.component.html',
+  // templateUrl: '../../../../../../../app/community-page/sections/sub-com-col-section/sub-community-list/community-page-sub-community-list.component.html',
   standalone: true,
   imports: [
     CdkTreeModule,
