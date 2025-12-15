d1758b012307b75f5fd4293070f43f14b53dfae3
178Pelado <mosqueirafelipe22@gmail.com>
2025-12-15 10:43:20 -0300

    Saco el logo de dspace de la barra de admin. Dejo el espacio vacío porque otro logo de sedici sería redundante

--- src/themes/custom/app/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/themes/custom/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -12,8 +12,8 @@ import { BrowserOnlyPipe } from '../../../../../app/shared/utils/browser-only.pi
 
 @Component({
   selector: 'ds-themed-admin-sidebar',
-  // templateUrl: './admin-sidebar.component.html',
-  templateUrl: '../../../../../app/admin/admin-sidebar/admin-sidebar.component.html',
+  templateUrl: './admin-sidebar.component.html',
+  // templateUrl: '../../../../../app/admin/admin-sidebar/admin-sidebar.component.html',
   // styleUrls: ['./admin-sidebar.component.scss']
   styleUrls: ['../../../../../app/admin/admin-sidebar/admin-sidebar.component.scss'],
   standalone: true,
