271d78f8a3f4a580a7309ca0448b1cfbc03f7f53
178Pelado <mosqueirafelipe22@gmail.com>
2026-02-20 09:47:41 -0300

    Alíneo en la parte superior el desplegable de subcomunidades y colecciones

--- src/themes/custom/app/community-list-page/community-list/community-list.component.ts
+++ src/themes/custom/app/community-list-page/community-list/community-list.component.ts
@@ -10,7 +10,7 @@ import { ThemedLoadingComponent } from '../../../../../app/shared/loading/themed
 
 @Component({
   selector: 'ds-themed-community-list',
-  // styleUrls: ['./community-list.component.scss'],
+  styleUrls: ['./community-list.component.scss'],
   templateUrl: './community-list.component.html',
   // templateUrl: '../../../../../app/community-list-page/community-list/community-list.component.html',
   standalone: true,
