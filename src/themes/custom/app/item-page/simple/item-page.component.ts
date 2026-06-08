01f7589344999d6fa73a8dc8597740c7c5d4eeeb
Santiago Fierro <santiago.fierro99@gmail.com>
2026-06-08 09:42:09 -0300

    elimino importaciones no usadas para limpiar los warnings de la consola

--- src/themes/custom/app/item-page/simple/item-page.component.ts
+++ src/themes/custom/app/item-page/simple/item-page.component.ts
@@ -8,7 +8,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ThemedItemAlertsComponent } from '../../../../../app/item-page/alerts/themed-item-alerts.component';
 
 import { ItemPageComponent as BaseComponent } from '../../../../../app/item-page/simple/item-page.component';
-import { AccessByTokenNotificationComponent } from '../../../../../app/item-page/simple/access-by-token-notification/access-by-token-notification.component';
 import { NotifyRequestsStatusComponent } from '../../../../../app/item-page/simple/notify-requests-status/notify-requests-status-component/notify-requests-status.component';
 import { QaEventNotificationComponent } from '../../../../../app/item-page/simple/qa-event-notification/qa-event-notification.component';
 import { ItemVersionsComponent } from '../../../../../app/item-page/versions/item-versions.component';
@@ -29,13 +28,12 @@ import { ViewTrackerResolverService } from '../../../../../app/statistics/angula
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [fadeInOut],
   imports: [
-    AccessByTokenNotificationComponent,
     AsyncPipe,
     ErrorComponent,
     ItemVersionsComponent,
     ItemVersionsNoticeComponent,
     ListableObjectComponentLoaderComponent,
-    NotifyRequestsStatusComponent,
+    NotifyRequestsStatusComponent,  
     QaEventNotificationComponent,
     ThemedItemAlertsComponent,
     ThemedLoadingComponent,
--- src/themes/custom/app/search-page/configuration-search-page.component.ts
+++ src/themes/custom/app/search-page/configuration-search-page.component.ts
@@ -12,7 +12,6 @@ import { SearchConfigurationService } from '../../../../app/core/shared/search/s
 import { SEARCH_CONFIG_SERVICE } from '../../../../app/my-dspace-page/my-dspace-configuration.service';
 import { ConfigurationSearchPageComponent as BaseComponent } from '../../../../app/search-page/configuration-search-page.component';
 import { pushInOut } from '../../../../app/shared/animations/push';
-import { SearchLabelsComponent } from '../../../../app/shared/search/search-labels/search-labels.component';
 import { ThemedSearchResultsComponent } from '../../../../app/shared/search/search-results/themed-search-results.component';
 import { ThemedSearchSidebarComponent } from '../../../../app/shared/search/search-sidebar/themed-search-sidebar.component';
 import { ThemedSearchFormComponent } from '../../../../app/shared/search-form/themed-search-form.component';
@@ -37,7 +36,6 @@ import { ViewModeSwitchComponent } from '../../../../app/shared/view-mode-switch
     AsyncPipe,
     NgTemplateOutlet,
     PageWithSidebarComponent,
-    SearchLabelsComponent,
     ThemedSearchFormComponent,
     ThemedSearchResultsComponent,
     ThemedSearchSidebarComponent,
